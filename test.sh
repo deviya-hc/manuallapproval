9462e98ff102d305555a64c864776988c314c0e4
jeebanj <32644143+jeebanj@users.noreply.github.com>
2024-02-08 11:38:28 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Path to the file storing the version counter
+counter_file="version_counter.txt"
+
+# Read the current version from the file or use 0 as the default
+version=$(cat "$counter_file" 2>/dev/null) || version=0
+
+# Increment the version
+((version++))
+
+# Update the version counter file
+echo "$version" > "$counter_file"
